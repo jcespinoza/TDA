70b3631da8ef09366e7cfe71386decea3bd7de69
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-02-13 17:01:48 -0600

    Replaced old code in ListPointerT

--- listpointert.h
+++ listpointert.h
@@ -12,110 +12,150 @@ template <class T>
 class ListPointerT
 {
 public:
-    ListPointerT(int size = 10){
-        this->size = size;
+    int count;
+    NodeT<T>* firstN;
+    NodeT<T>* lastN;
+
+    ListPointerT(int i = 0){
         count = 0;
         firstN = 0;
         lastN = 0;
     }
 
     ~ListPointerT(){
-        while(count > 0)
-            remove(0);
-        lastN = 0;
-        if(lastN != 0)
-            delete lastN;
+        //code to release memory here
     }
-    int insert(int pos, T val){
-        if(pos < 0 || pos > count || pos > getSize() || count == getSize())
-            return -1;
 
-            NodeT<T>* n = new NodeT<T>();
-            n->value = val;
-
-        if(pos == 0){
-            n->next = firstN;
-            firstN = n;
-            if(count == pos)
-                lastN = firstN;
+    void insert(T val){
+        if(!isValid(val))
+            return;
+        NodeT<T>* newone = new NodeT<T>();
+        newone->value = val;
+        newone->next = 0;
+        if(firstN == 0){
+            firstN = newone;
             count++;
-            return 0;
-
-        }else if(pos == count){
-            n->next = 0;
-            lastN->next = n;
-            lastN = n;
-            count++;
-            return 0;
-
         }else{
             NodeT<T>* cursor = firstN;
-            for(int i = 0; i < pos; i++){
+            while(cursor->next != 0){
                 cursor = cursor->next;
-                if(cursor == 0){
-                    return -1;
-                }
             }
-            n->next = cursor->next;;
-            cursor->next = n;
+            cursor->next = newone;
             count++;
-            return 0;
         }
     }
 
-    int swap(int i, int j){
-        if( i < 0 || j < 0 || i > count || j > count
-                || i > getSize() || j > getSize())
-            return -1;
-
-        if(i == j)
-            return 0;
-
-        T first =  get(i);
-        T second = get(j);
-        goTo(i)->value = second;
-        goTo(j)->value = first;
-        return 0;
+    virtual bool isValid(T val){
+        return true;
     }
 
-    int shuffleItems(){
-        int currentR = 0;
-        int x = 0;
-        int y = getCount()-1;
-        for(int i = 0; i < getCount(); i++){
-            currentR = rand()%getCount();
-            if(currentR%2 == 0){
-                x = currentR;
-                y = (x + 1);
+    void release(){
+        while(count > 0)
+            remove(0);
+    }
+
+    void updateCount(){
+        int c = 0;
+        NodeT<T>* cursor = firstN;
+        while(cursor != 0){
+            cursor = cursor->next;
+            c++;
+        }
+        count = c;
+    }
+
+    void insert(int pos, T val){
+        if(!isValid(val))
+            return;
+
+        NodeT<T>* newone = new NodeT<T>();
+        newone->value = val;
+        newone->next = 0;
+        if(pos == 0){
+            if(firstN == 0){
+                firstN = newone;
+            }else{
+                newone->next = firstN;
+                firstN = newone;
             }
-            swap(x,y);
-            x = i;
-            y = x + 1;
+            count++;
+        }else{
+            NodeT<T>* cursor = goTo(pos-1);
+            if(cursor != 0){
+                newone->next = cursor->next;
+                cursor->next = newone;
+                count++;
+            }
         }
-        return 0;
     }
 
-    T first(){
-        if(firstN != NULL)
-            return firstN->value;
-        return 0;
+    void remove(int pos){
+        if(pos < 0 || pos >= count || firstN == 0)
+            return;
+        NodeT<T>* target = 0;
+        if(pos == 0 ){
+            target = firstN;
+            firstN = firstN->next;
+        }else{
+            NodeT<T>* previo = goTo(pos-1);
+            target = goTo(pos);
+            previo->next = target->next;
+        }
+        cout << "\nDeleted " << target->value;
+        delete target;
+        count--;
     }
 
-    T last(){
-        if(lastN != 0)
-            return lastN->value;
-        return 0;
+    NodeT<T>* disconnect(int pos){
+        if(pos < 0 || pos >= count || firstN == 0)
+            return 0;
+        NodeT<T>* target = 0;
+        if(pos == 0 ){
+            target = firstN;
+            firstN = firstN->next;
+        }else{
+            NodeT<T>* previo = goTo(pos-1);
+            target = goTo(pos);
+            previo->next = target->next;
+        }
+        return target;
+        count--;
     }
 
-    T next(int pos){
-        NodeT<T>* cursor = goTo(pos+1);
-        return (cursor == 0)?0:cursor->value;
+    NodeT<T>* disconnectFrom(int pos){
+        if(pos < 0 || pos >= count || firstN == 0)
+            return 0;
+        NodeT<T>* target = 0;
+        if(pos == 0){
+            target = firstN;
+            firstN = 0;
+            count = 0;
+            lastN = 0;
+        }else{
+            target = goTo(pos);
+            goTo(pos-1)->next = 0;
+        }
+        updateCount();
+        return target;
     }
 
-    int purge(){
+    NodeT<T>* disconnectLast(){
+        return disconnectFrom(count-1);
+    }
+
+    void append(NodeT<T>* node){
+        if(firstN == 0)
+            firstN = node;
+        else{
+            last()->next = node;
+        }
+        updateCount();
+    }
+
+    void purge(){
         int p = 0;
         int q;
-        while( p < count -1 ){
+        while( p < count-1 ){
             q = p + 1;
             while(q <= count-1){
                 if(get(p) == get(q))
@@ -125,7 +165,6 @@ public:
             }
             p++;
         }
-        return 0;
     }
 
     int removeDuplicates(T val){
@@ -143,185 +182,97 @@ public:
         return 0;
     }
 
-    int getIndex(T val){
-        for(int i = 0; i < count; i++){
-            if(get(i) == val)
-                return i;
-        }
-        return -1;
+    int getCount(){
+        return count;
     }
 
-    int remove(int pos){
-        if(pos < 0 || pos > count || pos > size)
-            return -1;
-
-        if(pos == 0){
-            NodeT<T>* target = firstN;
-            if(firstN != 0)
-                firstN = firstN->next;
-            count--;
-            target->next = 0;
-            delete target;
-            return 0;
-
-        }else if(pos == count-1){
-            NodeT<T>* target = firstN;
-            NodeT<T>* targPrev = 0;
-
-            while(target-> next != 0){
-                targPrev = target;
-                target = target->next;
-            }
-            lastN = targPrev;
-            targPrev->next = 0;
-            delete target;
-            count--;
-            return 0;
-        }else{
-            NodeT<T>* target = firstN;
-            NodeT<T>* targPrev = 0;
-
-            for(int i = 0; i < pos; i++){
-                targPrev = target;
-                target = target->next;
-            }
-            targPrev->next = target->next;
-            delete target;
-            count--;
-            return 0;
-        }
+    NodeT<T>* first(){
+        return firstN;
     }
 
-
-    int printList(){
-        cout << "\nLIST:";
+    NodeT<T>* last(){
+        if(firstN == 0)
+            return 0;
         NodeT<T>* cursor = firstN;
-        int i = 0;
-        while(cursor != 0){
-            cout << "\nelement[" << i++ << "]:" << cursor->value;
+        while(cursor->next != 0){
             cursor = cursor->next;
+        }
+        return lastN = cursor;
+    }
+
+    void printList(){
+        NodeT<T>* cursor = firstN;
+        int c = 0;
+        while(cursor != 0){
+            cout << "\nElement " << c << ": " << cursor->value;
+            cursor = cursor->next;
+            c++;
+        }
+    }
+
+    NodeT<T>* goTo(int pos){
+        if(pos < 0 || pos >= count)
+            return 0;
+        NodeT<T>* cursor = firstN;
+        int c = 0;
+        while(cursor != 0){
+            if(c == pos)
+                return cursor;
+            cursor = cursor->next;
+            c++;
         }
         return 0;
     }
 
-    void setSize(int i){
-        if(i > 0)
-            size = i;
-        firstN = 0;
-        lastN = 0;
-    }
-
     T get(int pos){
-        if(pos < 0 || pos > count-1 || pos > size)
-            return 0;
-        NodeT<T>* cursor = firstN;
-        for(int i = 0; i < pos; i++){
-            cursor = cursor->next;
-        }
+        NodeT<T>* cursor = goTo(pos);
+        if(cursor == 0)
+            return T();
         return cursor->value;
     }
 
-    int getCount(){return count;}
-    int getSize(){return size;}
 
-    NodeT<T>* goTo(int pos){
-        if(pos < 0 || pos > count-1 || pos > size)
-            return 0;
+    int getIndex(T val){
+        int i = 0;
         NodeT<T>* cursor = firstN;
-        for(int i = 0; i < pos; i++){
+        while(cursor != 0){
+            if(cursor->value == val)
+                return i;
+            i++;
             cursor = cursor->next;
         }
-        return cursor;
+        return -1;
     }
 
-    NodeT<T>* getFirst(){
-        return this->firstN;
-    }
-    NodeT<T>* getLast(){
-        return this->lastN;
-    }
 
-    void append(NodeT<T>* prime){
-        cout << "\nOK into append";
-        if(prime == 0)
+    void swap(int i, int j){
+        if( i < 0 || j < 0 || i >= count || j >= count)
             return;
-        cout << "\nprime was not null";
-        NodeT<T>* cursor = prime;
-        NodeT<T>* tempLast = lastN;
-        int hMany = 0;
-        while(cursor != 0){
-            hMany++;
-            tempLast = cursor;
-            cursor = cursor->next;
-        }
-        if(count == 0)
-            firstN = prime;
-        if(lastN != 0)
-            lastN->next = prime;
-        lastN = tempLast;
-        count += hMany;
+
+        if(i == j)
+            return;
+
+        T first =  get(i);
+        T second = get(j);
+        goTo(i)->value = second;
+        goTo(j)->value = first;
     }
 
-    NodeT<T>* disconnect(int from, int until){
-        if(count < 1 || from < 0 || until >= getCount() || from > until)
-            return 0;
-        cout << "\nWas right";
-        NodeT<T>* pre = 0;
-        NodeT<T>* fst = 0;
-        NodeT<T>* lst = 0;
-        NodeT<T>* pos = 0;
-        int hMany = 1 + until - from;
-        if(from == 0){
-            fst = firstN;
-            lst = goTo(until);
-            firstN = lst->next;
-            lst->next = 0;
-            if(until == getCount()-1)
-                lastN == firstN;
-
-        }else if(from == getCount()-1){
-            if(until == from){
-                pre = goTo(from -1);
-                if(pre != 0)
-                    pre->next = 0;
-                fst = goTo(from);
-                lastN = pre;
+    void shuffleItems(){
+        int currentR = 0;
+        int x = 0;
+        int y = getCount()-1;
+        for(int i = 0; i < getCount(); i++){
+            currentR = rand()%getCount();
+            if(currentR%2 == 0){
+                x = currentR;
+                y = (x + 1);
             }
-        }else{
-            pre = goTo(from -1);
-            fst = goTo(from);
-            lst = goTo(until);
-            pos = goTo(until+1);
-            pre->next = pos;
-            lst->next = 0;
+            swap(x,y);
+            x = i;
+            y = x + 1;
         }
-        count -= hMany;
-        if(count == 0)
-            firstN = lastN = 0;
-        return fst;
     }
-
-    NodeT<T>* disconnectLast(){
-        NodeT<T>* ret = 0;
-        if(count == 1)
-            return 0;
-        NodeT<T>* cursor = firstN;
-        while(cursor != 0){
-            ret = cursor->next;
-            cursor = cursor->next;
-        }
-        cursor->next = 0;
-        lastN = cursor;
-        count--;
-        return ret;
-    }
-
-private:
-    int size;
-    int count;
-protected:
-    NodeT<T>* firstN;
-    NodeT<T>* lastN;
 };
 
 #endif // LISTPOINTERT_H
